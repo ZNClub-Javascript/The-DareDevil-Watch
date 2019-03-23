b867c3b107a30b2ddcd241245abde5f68134db49
ZNevzz <dsouza.nevil45@gmail.com>
2019-03-23 13:10:47 +0530

    banner updated

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
-
+// const fetch = require('fetch');
+const axios = require('axios');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+
+
+exports.fetchBmsPage = functions.https.onRequest((request, response) => {
+	response.send("Hello from Firebase!");
 });
