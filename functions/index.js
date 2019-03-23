d36bbda954bb021e56269714cc16431227b4270c
ZNevzz <dsouza.nevil45@gmail.com>
2019-03-23 17:24:30 +0530

    blob forwarded

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,37 @@
 const functions = require('firebase-functions');
-// const fetch = require('fetch');
+const fetch = require('fetch');
 const axios = require('axios');
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 
+handleResponse = (axResponse, response) => {
+
+	response.send(axResponse.data);
+}
+
+handleError = (error) => {
+	console.log(error);
+	throw error;
+}
+
+const url = 'https://in.bookmyshow.com/sports/ipl-mumbai-indians/ET00097338';
+
+const options = {
+  host: 'eternagame.wikia.com',
+  path: '/sports/ipl-mumbai-indians/ET00097338'
+};
+
+const urlOptions = {
+  /*transformResponse: [(data) => {
+  	console.log(typeof data); 
+  }],*/
+  responseType: 'blob'
+};
 
 
 exports.fetchBmsPage = functions.https.onRequest((request, response) => {
-	response.send("Hello from Firebase!");
+	// axios.get(url)
+	axios(url, urlOptions)
+		.then( (axResponse) => handleResponse(axResponse, response))
+		.catch( handleError );
+	
 });
